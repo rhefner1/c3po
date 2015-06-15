145e8b3569c5caf95fd627207f530efc1e523b36
Ryan Hefner <ryan.hefner@netapp.com>
2015-06-15 00:04:14 -0400

    More cosmetic changes
    Remove the extra newline in all of the CI output.

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -5,5 +5,5 @@ export PYTHONPATH="${PYTHONPATH}:$(pwd)/ci/google_appengine"
 export PYTHONPATH="${PYTHONPATH}:$(pwd)/ci/google_appengine/lib/yaml-3.10"
 echo "Current PYTHONPATH: ${PYTHONPATH}"
 
-echo -e "\n\n### Running unit tests"
+echo -e "\n### Running unit tests"
 python -m unittest discover
