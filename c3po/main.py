f58d7e380fc8d89ba8bc64144fcfa5f20bf4c01e
Ryan Hefner <ryan.hefner@netapp.com>
2015-06-20 19:09:57 -0400

    Fix initial Pylint Issues
    Pylint was recently added to CI. Fixing the problems that it's noting.

--- c3po/main.py
+++ c3po/main.py
@@ -1,15 +1,18 @@
-from flask import abort, Flask, request
+"""Handles the web routing for incoming API calls"""
+
 import logging
+
+from flask import abort, Flask, request
 
 from c3po.message import Message
 
 SUCCESS = ("", 200)
 
-app = Flask(__name__)
-app.config['DEBUG'] = True
+APP = Flask(__name__)
+APP.config['DEBUG'] = True
 
 
-@app.route('/', methods=['POST'])
+@APP.route('/', methods=['POST'])
 def message():
     """Processes a message and returns a response."""
     group_id = request.form['group_id']
@@ -19,9 +22,9 @@ def message():
     if not group_id or not name or not text:
         abort(400)
 
-    logging.info("Group ID: %s" % group_id)
-    logging.info("Name: %s" % name)
-    logging.info("Text: %s" % text)
+    logging.info("Group ID: %s", group_id)
+    logging.info("Name: %s", name)
+    logging.info("Text: %s", text)
 
     msg = Message(group_id, name, text)
 
@@ -33,13 +36,13 @@ def message():
     return SUCCESS
 
 
-@app.route('/test')
+@APP.route('/test')
 def test():
     """Says hello!"""
     return "Hello!"
 
 
-@app.errorhandler(404)
-def page_not_found(e):
+@APP.errorhandler(404)
+def page_not_found(error):
     """Return a custom 404 error."""
     return 'Sorry, nothing at this URL.', 404
--- c3po/message.py
+++ c3po/message.py
@@ -1,8 +1,10 @@
+"""Contains the data model for a Message and the actions to process it."""
+
 import logging
 import re
 
 from c3po import responders
-from settings import Settings
+from c3po import settings
 
 MENTIONED_MAP = {
     "(hi|hello)": responders.hello
@@ -12,12 +14,12 @@ NOT_MENTIONED_MAP = {
 
 }
 
-REGEX_MENTIONED = '(C-3PO|c3po)'
-REGEX_PRE = '(\s+)'
-REGEX_POST = '($|\s+|\?+|\.+|\!+)'
+REGEX_MENTIONED = r'(C-3PO|c3po)'
+REGEX_PRE = r'(\s+)'
+REGEX_POST = r'($|\s+|\?+|\.+|\!+)'
 
 
-class Message:
+class Message(object):
     """Holds the message contents, processes it and sends a response."""
 
     def __init__(self, group_id, name, text):
@@ -27,36 +29,49 @@ class Message:
 
     @staticmethod
     def _generate_regex(regex):
+        """Generates the regex needed to find the correct responder."""
         return "%s%s%s" % (REGEX_PRE, regex, REGEX_POST)
 
     def _get_bot_id(self):
-        results = Settings.query(Settings.group_id == self.group_id)
+        """Returns the bot_id corresponding to the given group_id."""
+        results = settings.Settings.query(settings.Settings.group_id ==
+                                          self.group_id)
         if results.count() == 0:
-            raise ValueError("No Settings objects found matching group_id: %s. Cannot continue." % self.group_id)
+            raise ValueError(
+                "No Settings objects found matching group_id: %s. Cannot"
+                "continue." % self.group_id)
         elif results.count() > 1:
-            raise RuntimeError("Found multiple Settings objects with group_id: %s. Cannot continue." % self.group_id)
+            raise RuntimeError(
+                "Found multiple Settings objects with group_id: %s. Cannot"
+                "continue." % self.group_id)
 
         return results.fetch(1)[0]
 
     def process_message(self):
+        """Finds the responder and uses it to send a response."""
         # Check for matches where C-3PO is mentioned
         if re.search(REGEX_MENTIONED, self.text):
-            self._find_responder(MENTIONED_MAP)
+            responder = self._find_responder(MENTIONED_MAP)
 
         else:
             # Now checking for the rest of the cases
-            self._find_responder(NOT_MENTIONED_MAP)
+            responder = self._find_responder(NOT_MENTIONED_MAP)
+
+        response = responder(self)
+        self._send_response(response)
 
     def _find_responder(self, responder_map):
+        """Returns the first responder that is registered for the given
+        incoming text."""
         for regex, responder in responder_map.iteritems():
             if re.search(self._generate_regex(regex), self.text):
-                logging.info("Responder selected: %s" % str(responder))
-                response = responder(self)
-                self._send_response(response)
+                logging.info("Responder selected: %s", str(responder))
+                return responder
 
     def _send_response(self, response):
-        logging.info("Sending this response: %s" % response)
+        """Sends the given response to the API."""
+        logging.info("Sending this response: %s", response)
 
         bot_id = self._get_bot_id()
-        logging.info("Sending to bot_id: %s" % bot_id)
+        logging.info("Sending to bot_id: %s", bot_id)
         return
