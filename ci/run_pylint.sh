1a65a21eb1101fc15f829a3082412977ae2d69ec
Ryan Hefner <ryan.hefner@netapp.com>
2015-06-21 16:03:42 -0400

    Run unit test and pylint in separate jobs
    Currently, unit tests and pylint run in the same job. It would be
    helpful for these to run in different jobs to get a sense of why
    a failure is seen. Also, this allows the two jobs to run concurrently.

---
+++ ci/run_pylint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [[ $RUN_PYLINT == true ]]
+then
+    echo -e "### Adding SDK to Python path"
+    export PYTHONPATH="${PYTHONPATH}:$(pwd)/ci/google_appengine"
+    export PYTHONPATH="${PYTHONPATH}:$(pwd)/ci/google_appengine/lib/yaml-3.10"
+    echo "Current PYTHONPATH: ${PYTHONPATH}"
+
+    echo -e "\n### Running pylint"
+    pylint c3po --disable=no-member --disable too-few-public-methods --disable unused-argument
+    pylint tests --disable=no-member --disable too-few-public-methods --disable unused-argument --disable missing-docstring --disable protected-access
+fi
