630527162c55f01441c361f7d706c143b2036eed
Ryan Hefner <rhefner1@gmail.com>
2015-08-17 19:15:39 -0400

    ClarkAlert
    When `clark` is mentioned, we go see if Clark is closed. If it is
    open, we return the current menu options depending on the time.
    
    Fixes #23

--- tests/response_mgr/test_small_group.py
+++ tests/response_mgr/test_small_group.py
@@ -1,9 +1,61 @@
+import json
 import unittest
 
+import freezegun
 import mock
 
 from c3po.provider.groupme import send
 from tests import fakes
+from c3po.response_mgr import small_group
+
+
+class TestClarkGetCurrentMeal(unittest.TestCase):
+    @freezegun.freeze_time('2015-01-01 10:00:00', tz_offset=+5)
+    def test_breakfast(self):
+        expected = 'breakfast'
+        self.assertEqual(small_group.get_current_meal(), expected)
+
+    @freezegun.freeze_time('2015-01-01 13:00:00', tz_offset=+5)
+    def test_lunch(self):
+        expected = 'lunch'
+        self.assertEqual(small_group.get_current_meal(), expected)
+
+    @freezegun.freeze_time('2015-01-01 19:00:00', tz_offset=+5)
+    def test_dinner(self):
+        expected = 'dinner'
+        self.assertEqual(small_group.get_current_meal(), expected)
+
+
+class TestClarkClosed(unittest.TestCase):
+    @freezegun.freeze_time('2015-01-01 06:00:00', tz_offset=+5)
+    def test_closed_early(self):
+        expected = True
+        self.assertEqual(small_group.is_clark_closed(), expected)
+
+    @freezegun.freeze_time('2015-01-01 23:00:00', tz_offset=+5)
+    def test_closed_late(self):
+        expected = True
+        self.assertEqual(small_group.is_clark_closed(), expected)
+
+    @mock.patch('google.appengine.api.urlfetch.fetch')
+    @freezegun.freeze_time('2015-01-01 12:00:00', tz_offset=+5)
+    def test_closed_all_day(self, fake_fetch):
+        fake_api_return = mock.Mock()
+        fake_api_return.content = fakes.CLARK_CLOSED
+        fake_fetch.return_value = fake_api_return
+
+        expected = True
+        self.assertEqual(small_group.is_clark_closed(), expected)
+
+    @mock.patch('google.appengine.api.urlfetch.fetch')
+    @freezegun.freeze_time('2015-01-01 12:00:00', tz_offset=+5)
+    def test_open(self, fake_fetch):
+        fake_api_return = mock.Mock()
+        fake_api_return.content = fakes.CLARK_OPEN
+        fake_fetch.return_value = fake_api_return
+
+        expected = False
+        self.assertEqual(small_group.is_clark_closed(), expected)
 
 
 class TestSmallGroupResponders(unittest.TestCase):
@@ -29,6 +81,20 @@ class TestSmallGroupResponders(unittest.TestCase):
         self.msg.process_message()
 
         self.mock_send.assert_called_with('Got it.')
+
+    @mock.patch('c3po.response_mgr.small_group.get_clark_menu_items')
+    @mock.patch('c3po.response_mgr.small_group.get_current_meal')
+    @mock.patch('c3po.response_mgr.small_group.is_clark_closed')
+    def test_clark(self, fake_clark_closed, fake_current_meal, fake_clark_menu):
+        fake_clark_closed.return_value = False
+        fake_current_meal.return_value = 'dinner'
+        fake_clark_menu.return_value = json.loads(fakes.CLARK_MENU)
+
+        self.msg.text = 'clark'
+        self.msg.process_message()
+
+        response = "ClarkAlert for dinner: Chicken."
+        self.mock_send.assert_called_with(response)
 
     def test_gather_prayer_requests(self):
         self.msg.text = 'c3po gather prayer requests'
